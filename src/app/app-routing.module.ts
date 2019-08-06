b3f8d5409501c50e8374336b7f1da9b6e09552f6
digitamo <osama.khaled.saad@gmail.com>
2019-08-06 18:28:30 +0200

    feat: use directive in preloaded components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RulesComponent } from './rules/rules.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomeModule' },
-  { path: 'rules', loadChildren: './rules/rules.module#RulesModule' }
+  { path: 'home', component: HomeComponent },
+  { path: 'rules', component: RulesComponent }
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RulesComponent } from './rules/rules.component';
+import { HomeComponent } from './home/home.component';
+import { BaseModule } from './base/base.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RulesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BaseModule
   ],
   providers: [],
   exports: [
